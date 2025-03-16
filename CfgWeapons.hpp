7332b7b5287b497b2bf24c74fc990a58520dfb8d
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-03-16 10:35:10 +0000

    GX AMR Fix

--- CfgWeapons.hpp
+++ CfgWeapons.hpp
@@ -2,7 +2,7 @@ class CfgWeapons {
 	class GM6_base_F;
 	class GX_AMR_MK1_Weapon: GM6_base_F {
 		magazines[] = {"TAW_AMMO_10RND_127x99_MAG","TAW_AMMO_10RND_127x99_TRACER_MAG","TAW_AMMO_10RND_127x99_API_MAG"};
-		magazineWell[] = {"CBA_50BMG_M107"};
+		//magazineWell[] = {"CBA_50BMG_M107"};
 	};
 	class GX_AMR_MK2_Weapon: GX_AMR_MK1_Weapon {
 		magazines[] = {"TAW_AMMO_20RND_338_LM_MAG","TAW_AMMO_20RND_338_LM_API_MAG"};
